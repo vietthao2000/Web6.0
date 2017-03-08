09093b4d490b7dbab0cd3e6c46322c1e6e05ba3d
vietthao2000 <vietthao2000@gmail.com>
2017-03-08 21:47:46 +0700

    Initial commit

--- js/main.js
+++ js/main.js
@@ -31,11 +31,24 @@ var preload = function(){
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
   Nakama.keyboard = Nakama.game.input.keyboard;
+  Nakama.game.add.sprite(0,0,'background')
+  Nakama.player = Nakama.game.add.sprite(200,200,'assets','Spaceship1-Player.png');
 }
 
 // update game state each frame
 var update = function(){
-
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
+    if (Nakama.player.position.y-10>=0) {Nakama.player.position.y -= 10;}
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    if (Nakama.player.position.y+10<=880) {Nakama.player.position.y += 10;}
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    if (Nakama.player.position.x-10>=0) {Nakama.player.position.x -= 10;}
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    if (Nakama.player.position.x+10<=560) {Nakama.player.position.x += 10;}
+  }
 }
 
 // before camera render (mostly for debug)
