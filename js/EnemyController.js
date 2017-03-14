bab89c977e906733ff0def786c6851d0830688d1
vietthao2000 <vietthao2000@gmail.com>
2017-03-15 00:39:36 +0700

    Fix performance issue

--- js/EnemyController.js
+++ js/EnemyController.js
@@ -3,6 +3,7 @@ class EnemyController {
 		this.sprite = Nakama.game.add.sprite(x, y, 'assets',spriteName);  
 		Nakama.game.physics.arcade.enable(this.sprite);
 		this.configs = configs;
+		this.sprite.body.velocity.x = this.configs.initialSpeed*((Math.random()>0.5)?1:-1);
 	}
 
 	update() {
@@ -11,9 +12,6 @@ class EnemyController {
 		}
 		else if (this.sprite.position.x>=this.configs.right) {
 			this.sprite.body.velocity.x = -this.configs.initialSpeed;
-		}
-		if (this.sprite.body.velocity.x==0) {
-			this.sprite.body.velocity.x = this.configs.initialSpeed*((Math.random()>0.5)?1:-1);
 		}
 	}
 }
--- js/ShipController.js
+++ js/ShipController.js
@@ -1,18 +1,16 @@
 class ShipController {
 	constructor(x, y, spriteName, configs) {
-		this.bulletConfigs = {
-		  asset: ["BulletType1.png","BulletType2.png","BulletType3.png"],
-		  speed: [400,400,4000],
-		  fireRate: [60,60,500],
-		  x: [40,40,40],
-		  y: [0,0,-400],
-		}
-
-		this.i=0;
 		this.sprite = Nakama.game.add.sprite(x, y, 'assets',spriteName);  
 		Nakama.game.physics.arcade.enable(this.sprite);
 		this.configs = configs;
-		//this.createBullet();
+		this.bulletConfigs = {
+			asset 	: ["BulletType1.png","BulletType2.png","BulletType3.png"],
+			speed 	: [400,400,4000],
+			fireRate 	: [60,60,500],
+			x 		: [40,40,40],
+			y 		: [0,0,-400],
+		}
+		this.createBullet();
 	}
 
 	update() {
@@ -36,30 +34,31 @@ class ShipController {
 			this.sprite.body.velocity.x = 0;
 		}
 
-		/*if (Nakama.keyboard.isDown(this.configs.SPACEBAR)) {
+		if (Nakama.keyboard.isDown(this.configs.SPACEBAR)) {
 			this.bullet.fire();
 		}
 
 		if (Nakama.keyboard.isDown(this.configs.E)) {
 			createBullet();
-		}*/
+		}
 	}
 
-	/*createBullet () {
-	  //Load the bullet asset image
-	  this.bullet = Nakama.game.add.weapon(100,'assets',this.bulletConfigs.asset[this.i]);
-	  //Kill the bullet when it goes out of the map
-	  this.bullet.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-	  //Set firing speed
-	  this.bullet.bulletSpeed = this.bulletConfigs.speed[this.i];
-	  //Set firing rate, 1 bullet per x milisecs
-	  this.bullet.fireRate = this.bulletConfigs.fireRate[this.i];
-	  //Rotate the bullet to make it point upward
-	  this.bullet.bulletAngleOffset = 90;
-	  //Track the Sprite and center it by horizontal offset
-	  this.bullet.trackSprite(this, this.bulletConfigs.x[this.i], this.bulletConfigs.y[this.i]);
-	  this.i=(this.i+1)%3;
-	}*/
+	createBullet () {
+		//Load the bullet asset image
+		this.bullet = Nakama.game.add.weapon(100,'assets',this.bulletConfigs.asset[this.i]);
+		this.bullet.i=0;
+		//Kill the bullet when it goes out of the map
+		this.bullet.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+		//Set firing speed
+		this.bullet.bulletSpeed = this.bulletConfigs.speed[this.bullet.i];
+		//Set firing rate, 1 bullet per x milisecs
+		this.bullet.fireRate = this.bulletConfigs.fireRate[this.bullet.i];
+		//Rotate the bullet to make it point upward
+		this.bullet.bulletAngleOffset = 90;
+		//Track the Sprite and center it by horizontal offset
+		this.bullet.trackSprite(this, this.bulletConfigs.x[this.bullet.i], this.bulletConfigs.y[this.bullet.i]);
+		this.bullet.i=(this.i+1)%3;
+	}
 }
 
 ShipController.SHIP_SPEED=400;
